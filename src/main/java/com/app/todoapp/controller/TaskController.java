217bbf69d40e09ef3470e1c1462a93309c81243b
AlenKuriakose17 <alenkuriakose1217@gmail.com>
2025-02-23 22:36:27 +0530

    Progress

--- src/main/java/com/app/todoapp/controller/TaskController.java
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -1,11 +1,17 @@
 package com.app.todoapp.controller;
 
 
+import com.app.todoapp.models.Task;
 import com.app.todoapp.services.TaskService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
-
+//@RequestMapping("/tasks")
 public class TaskController {
 
     private final TaskService taskService;
@@ -13,5 +19,13 @@ public class TaskController {
 
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
+    }
+
+    @GetMapping
+    public String getTasks(Model model){
+
+        List<Task> tasks = taskService.getAllTasks();
+        model.addAttribute("tasks",tasks);
+        return "Task";
     }
 }
